06f37bf9bc1b8153a7466f8f87b6816cf18bdd83
Kesavamoorthi Subramanian <k7moorthi@gmail.com>
2020-09-07 15:09:44 +0530

    feat(build): bump version before build
    BREAKING CHANGE: Bump version before build

---
+++ bump-version.js
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const semanticRelease = require('semantic-release');
+
+async function init() {
+  try {
+    const result = await semanticRelease({
+      branches: [
+        'stable',
+        '+([0-9])?(.{+([0-9]),x}).x',
+        {name: 'beta', prerelease: true},
+        {name: 'alpha', prerelease: true}
+      ],
+      plugins: [
+        '@semantic-release/commit-analyzer',
+        ['@semantic-release/npm', {
+          npmPublish: false
+        }]
+      ]
+    });
+
+    if (result) {
+      console.log(`Bumped to version ${result.nextRelease.version}.`);
+    } else {
+      console.log('No changes for version bump.');
+    }
+  } catch (err) {
+    console.error('Version bump failed with %O', err)
+  }
+}
+
+init();
